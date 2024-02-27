64ae7c62099530e5ce0a49cf79ec2b5352e6886e
SOULGAMER65 <94155734+SOULGAMER65@users.noreply.github.com>
2024-02-27 19:30:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching 🍓Frawlah🍓","Playing in 🍓Frawlah🍓"];
 
 
 let currentIndex = 0;
